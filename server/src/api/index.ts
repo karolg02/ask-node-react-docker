5302746653830018b4cd20283f2c290bf5f79fe6
Karol <karolglanowski02@gmail.com>
2024-06-02 00:03:31 +0000

    done

--- server/src/api/index.ts
+++ server/src/api/index.ts
@@ -5,7 +5,7 @@ const router = Router();
 
 router.get("/", (req: Request, res: Response) => {
   res.status(200).json({
-    text: "Hello world!",
+    text: "skdrjoiwejrioj!",
     params: req.query,
   });
 });
